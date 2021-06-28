16768b663bc510892ffe4b02577955febc8f1616
Trevor Bruce <tbnyk03@gmail.com>
2021-06-28 17:04:06 -0600

    Fixing Routes

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,19 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
+
+router.get("/api/workouts", (req,res) =>{
+    Workout.aggregate([
+        {
+            $addFields: {
+                workoutDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ]).then((workoutsDb) => {
+        res.json(workoutsDb);   
+    }).catch((err) => {
+        console.log(err)
+        res.json(err);
+    }); 
+});
