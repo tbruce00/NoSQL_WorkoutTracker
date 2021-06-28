e891baf2c22cdd37524869baa0d0f7a65816e911
Trevor Bruce <tbnyk03@gmail.com>
2021-06-28 16:42:19 -0600

    Create Heroku App

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 app.use(require("./routes/api"));
+app.use(require("./routes/views"));
 
 app.listen(PORT, () => {
     console.log(`The app is running on port ${PORT}!`);
