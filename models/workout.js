87c2b25b63953b0054850af95b39dcb38930c79e
Trevor Bruce <tbnyk03@gmail.com>
2021-06-28 18:38:29 -0600

    Update Server.js with heroku url

--- models/workout.js
+++ models/workout.js
@@ -36,7 +36,7 @@ const workoutSchema = new Schema({
         }
     ],
     workoutDuration: {
-        type: number,
+        type: Number,
     },
 });
 
--- routes/api.js
+++ routes/api.js
@@ -26,3 +26,35 @@ router.post("/api/workouts", (req,res) => {
         res.status(400).json(err);
     });
 });
+
+router.put('/api/workouts/:id', async (req,res) => {
+    try {
+        const workout = await Workout.findById(req.params.id)
+        workout.exercises.push(req.body)
+        await workout.save()
+        res.status(200).json(workout)     
+    }
+    catch (err) {
+        console.log(err)
+        res.status(400).json(err)
+    }
+});
+
+router.get('/api/workouts/range', (req,res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                workoutDuration: {
+                    $sum: '$exercises.duration',
+                }
+            }
+        }
+    ]).then(workoutsDb => {
+        res.json(workoutsDb);
+    }).catch(err => {
+        console.log(err),
+        res.json(err);
+    });
+});
+
+module.exports = router;
