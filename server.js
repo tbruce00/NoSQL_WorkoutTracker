69087e1f3f50627b13f63abc318c8e9a67e93453
Trevor Bruce <tbnyk03@gmail.com>
2021-06-29 11:43:18 -0600

    Fixed Directory Path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app,use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
